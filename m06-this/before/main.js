e829a492c42a9f1166f48f031411681d465aff70
qaisstan <94216834+qaisstan@users.noreply.github.com>
2024-10-27 16:05:46 +0100

    new changes

--- m06-this/before/main.js
+++ m06-this/before/main.js
@@ -1,6 +1,20 @@
-function printThis() {
-      "Use Strict"
-  console.log(this);
+let boeng747 = {
+  make: "Boeing 747",
+  capacity: 450,
+
+  print() {
+    console.log(this);
+  },
+};
+boeng747.print();
+
+function alertPassanger(name) {
+  console.log(`${name}: ${this.message}`);
 }
 
-printThis();
+const presentToGatreWarning = {
+  message: "Please present to boarding gate 3",
+  priority: 1,
+};
+
+alertPassanger(presentToGatreWarning, "Qais Stanikzai");
