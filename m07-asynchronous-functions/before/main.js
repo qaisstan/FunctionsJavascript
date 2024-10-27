295bdcf1dd41cbde1dca7d70a304a84a3554b8ce
qaisstan <94216834+qaisstan@users.noreply.github.com>
2024-10-27 16:54:43 +0100

    new changes

--- m07-asynchronous-functions/before/main.js
+++ m07-asynchronous-functions/before/main.js
@@ -1,43 +1,58 @@
+import fetch from "node-fetch";
+
 const createFlightEntry = (flight, from, status, ...args) => {
-    console.log(args);
+  console.log(args);
 
-    const row = document.createElement("tr");
-    if(args.length > 0){
-        const color = args[0];
-        row.style.color = color;
-    }
+  const row = document.createElement("tr");
+  if (args.length > 0) {
+    const color = args[0];
+    row.style.color = color;
+  }
 
-    const flightCol = document.createElement('td');
-    flightCol.innerText = flight;
+  const flightCol = document.createElement("td");
+  flightCol.innerText = flight;
 
-    const fromCol = document.createElement('td');
-    fromCol.innerText = from;
+  const fromCol = document.createElement("td");
+  fromCol.innerText = from;
 
-    const statusCol = document.createElement ('td');
-    statusCol.innerText = status;
+  const statusCol = document.createElement("td");
+  statusCol.innerText = status;
 
-    row.appendChild(flightCol);
-    row.appendChild(fromCol);
-    row.appendChild(statusCol);
+  row.appendChild(flightCol);
+  row.appendChild(fromCol);
+  row.appendChild(statusCol);
 
-    const parent = document.getElementById('arrivalList');
-    parent.appendChild(row);
-}
+  const parent = document.getElementById("arrivalList");
+  parent.appendChild(row);
+};
 
+const getFlights = () => {
+  return fetch("flight-summary.json");
+};
 
+getFlights()
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((f) => {
+      getFlightDetails(f.flight).then((details) => {
+        createFlightEntry(f.flight, f.from, details.status);
+      });
+    });
+  });
 
-createFlightEntry('RO12345', 'Paris', 'On time');
-createFlightEntry('US67957', 'Berlin', 'On time');
-createFlightEntry('AF89756', 'New York', 'Delayed', 'yellow');
-createFlightEntry('UAL7897', 'Bucharest', 'Cancelled', 'red');
-createFlightEntry('OS4782', 'Vienna', 'On time');
+  
+const getFlightDetails = (flightNumber) => {
+  return fetch("flight-details.json")
+    .then((res) => res.json())
+    .then((data) => data.find((f) => f.flight === flightNumber));
+};
 
 const displayTime = () => {
-    const time = new Date().toLocaleTimeString();
-    document.getElementById('time').innerText = time;
-}
+  const time = new Date().toLocaleTimeString();
+  document.getElementById("time").innerText = time;
+};
 
 displayTime();
 setInterval(() => {
-    displayTime();
-}, 1*1000);
+  displayTime();
+}, 1 * 1000);
